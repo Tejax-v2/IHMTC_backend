ede82945f02de9c6a0996fee4e862a5f3cdf2170
Tejax-v2 <tejastupke74@gmail.com>
2023-08-21 17:38:55 +0530

    Loginsystem Inputs received

--- loginsystem/views.py
+++ loginsystem/views.py
@@ -4,19 +4,49 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "loginsystem/home.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/home.html")
 
 def signup(request):
-    return render(request, "loginsystem/signup.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/signup.html")
+    elif request.method == "POST":
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+        print(email, pass1, pass2)
+        return HttpResponse("Signup Successful")
 
 def signin(request):
-    return render(request, "loginsystem/signin.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/signin.html")
+    elif request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(email, password)
+        return HttpResponse("Signin Successful")
 
 def forgot_password(request):
-    return render(request, "loginsystem/forgot-password.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/forgot-password.html")
+    elif request.method == "POST":
+        email = request.POST.get("email")
+        print(email)
+        return HttpResponse("Password reset link sent to your email") 
 
 def reset_password(request):
-    return render(request, "loginsystem/reset-password.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/reset-password.html")
+    elif request.method == "POST":
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+        print(pass1, pass2)
+        return HttpResponse("Password reset successful")
 
 def control_panel(request):
-    return render(request, "loginsystem/control-panel.html")
+    if request.method == "GET":
+        return render(request, "loginsystem/control-panel.html")
+    elif request.method == "POST":
+        email = request.POST.get("email")
+        print(email)
+        return HttpResponse("Control Panel")
