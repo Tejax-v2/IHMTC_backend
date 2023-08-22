03d5b8217f54783258cf9ec30607a4877a069302
Tejax-v2 <tejastupke74@gmail.com>
2023-08-22 22:38:36 +0530

    Participant Model and ModelAdmin created

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Participant
 
 # Register your models here.
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ["full_name","email"]
+
+
+admin.site.register(Participant,ParticipantAdmin)
